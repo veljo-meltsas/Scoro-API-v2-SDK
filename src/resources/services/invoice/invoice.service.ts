0f34febfcd0ee787048abf61fc8c4404bc2c86ab
André Vitor Rampanelli <andre.rampanelli@scoro.com>
2024-04-22 16:24:01 +0300

    feat: add Invoices services

---
+++ src/resources/services/invoice/invoice.service.ts
@@ -0,0 +1,38 @@
+import { APIClient } from '../../base'
+import { IDocPdf } from '../shared/types/doc-pdf.type'
+import { IInvoice } from './types/invoice.type'
+
+export class InvoicesService extends APIClient {
+  async getInvoice(id: number): Promise<IInvoice> {
+    return await this.view<IInvoice>('invoices', id)
+  }
+
+  async getInvoices(): Promise<IInvoice[]> {
+    return await this.list<IInvoice>('invoices')
+  }
+
+  async updateInvoice(
+    id: number,
+    data: Record<string, unknown>
+  ): Promise<void> {
+    await this.update('invoices', id, data)
+  }
+
+  async createInvoice(data: Partial<IInvoice>): Promise<IInvoice> {
+    return await this.create('invoices', data)
+  }
+
+  async deleteInvoice(id: number): Promise<void> {
+    await this.delete('invoices', id)
+  }
+
+  async findAllInvoicesBy(
+    filters: Record<string, unknown>
+  ): Promise<IInvoice[]> {
+    return await this.list<IInvoice>('invoices', filters)
+  }
+
+  async getPdf(id: number): Promise<IDocPdf> {
+    return await this.customCallWithouBodyAndId<IDocPdf>('invoices', 'pdf', id)
+  }
+}
---
+++ src/resources/services/shared/types/doc-pdf.type.ts
@@ -0,0 +1,5 @@
+export interface IDocPdf {
+  template_id: number
+  datetime_created: string // Date and time when the document was created (YYYY-mm-dd HH:ii:ss)
+  pdf_link: string // Link to the PDF document
+}
