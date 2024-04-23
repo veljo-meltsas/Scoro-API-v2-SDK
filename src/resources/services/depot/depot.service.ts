37e7af766d84b6983a66c6fe82953b99614ea20c
André Vitor Rampanelli <andre.rampanelli@scoro.com>
2024-04-23 16:42:14 +0300

    feat: add Depot service

---
+++ src/resources/services/depot/depot.service.ts
@@ -0,0 +1,16 @@
+import { APIClient } from '../../base'
+import type { IDepot } from './types/depot.type'
+
+export class DepotService extends APIClient {
+  async getDepot(id: number): Promise<IDepot> {
+    return await this.view<IDepot>('depot', id)
+  }
+
+  async getDepots(): Promise<IDepot[]> {
+    return await this.list<IDepot>('depot')
+  }
+
+  async findAllBy(query: Record<string, unknown>): Promise<IDepot[]> {
+    return await this.list<IDepot>('depot', query)
+  }
+}
---
+++ src/resources/services/depot/types/depot.type.ts
@@ -0,0 +1,5 @@
+export interface IDepot {
+  depot_id: number // Depot ID
+  depot_name: string // Depot name
+  comments: string // Depot comments/description
+}
