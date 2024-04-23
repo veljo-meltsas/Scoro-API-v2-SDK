99a34f89160a80b392e77453f595a025c58cc263
André Vitor Rampanelli <andre.rampanelli@scoro.com>
2024-04-23 14:22:02 +0300

    feat: add PurchaseOrder service

---
+++ src/resources/services/purchase-order/purchase-order.service.ts
@@ -0,0 +1,44 @@
+import { APIClient } from '../../base'
+import { IDocPdf } from '../shared/types/doc-pdf.type'
+import { IPurchaseOrder } from './types/purchase-order.type'
+
+export class PurchaseOrdersService extends APIClient {
+  async getPurchaseOrder(id: number): Promise<IPurchaseOrder> {
+    return await this.view<IPurchaseOrder>('purchaseOrders', id)
+  }
+
+  async getPurchaseOrders(): Promise<IPurchaseOrder[]> {
+    return await this.list<IPurchaseOrder>('purchaseOrders')
+  }
+
+  async updatePurchaseOrder(
+    id: number,
+    data: Record<string, unknown>
+  ): Promise<void> {
+    await this.update('purchaseOrders', id, data)
+  }
+
+  async createPurchaseOrder(
+    data: Partial<IPurchaseOrder>
+  ): Promise<IPurchaseOrder> {
+    return await this.create('purchaseOrders', data)
+  }
+
+  async deletePurchaseOrder(id: number): Promise<void> {
+    await this.delete('purchaseOrders', id)
+  }
+
+  async findAllPurchaseOrdersBy(
+    filters: Record<string, unknown>
+  ): Promise<IPurchaseOrder[]> {
+    return await this.list<IPurchaseOrder>('purchaseOrders', filters)
+  }
+
+  async getPdf(id: number): Promise<IDocPdf> {
+    return await this.customCallWithouBodyAndId<IDocPdf>(
+      'purchaseOrders',
+      'pdf',
+      id
+    )
+  }
+}
---
+++ src/resources/services/purchase-order/types/purchase-order.type.ts
@@ -0,0 +1,73 @@
+import { IAddress } from '../../shared/types/address.type'
+
+export interface IPurchaseOrder {
+  quote_id?: number // Related quote ID
+  order_id?: number // Related order ID
+  real_estate_id?: number // Related real estate ID
+  delivery_address_id?: number // Delivery address id
+  delivery_date?: string // Document delivery date (YYYY-mm-dd)
+  confirmed_date?: string // The date when document was confirmed (DATE_ISO8601 - Y-m-d\TH:i:sP)
+  modified_date?: string // The date when document was last modified (DATE_ISO8601 - Y-m-d\TH:i:sP)
+  currency_rate?: number // Exchange rate when purchase order is issued
+  id: number // Document ID
+  no: number // Document number
+  discount: number // Overall discount
+  discount2?: number | null // First additional discount
+  discount3?: number | null // Second additional discount
+  sum: number // Total sum after discounts without VAT
+  vat_sum: number // Total VAT sum
+  vat?: number // VAT percent
+  vat_code_id?: number // Invoice default vat_code_id
+  vat_code?: string // VAT code
+  company_id: number // ID of the related client
+  company_name?: string // Name of the related company
+  company_address?: IAddress // Object with address fields
+  person_id?: number // ID of the related contact person
+  person_name?: string // Name of the related contact person
+  company_address_id?: number // Company address ID
+  interested_party_id?: number // ID of the interested party
+  interested_party_address_id?: number // Interested party address ID
+  project_id?: number // ID of the related project
+  project_name?: string // Name of the related project
+  currency: string // Used currency
+  owner_id: number // User ID of the user that is responsible for the document
+  owner_email?: string // E-mail of user that is responsible for the document
+  date: string // Document date (YYYY-mm-dd)
+  status?: string // Status of the document
+  description?: string // Document description
+  account_id: string // Related business entity
+  is_sent?: boolean // If document is sent
+  lines?: IPurchaseOrderLine[] | null // Document lines
+  created_date?: string // The date and time when document was created (YYYY-mm-dd HH:ii:ss)
+  is_deleted?: boolean // Is deleted
+  deleted_date?: string // The date when object was deleted (DATE_ISO8601 - Y-m-d\TH:i:sP)
+  custom_fields?: Record<string, unknown> // Custom fields, only filled on view requests
+}
+
+interface IPurchaseOrderLine {
+  id: number // Document line ID
+  product_id: number // ID of the product
+  product_name?: string // Name of the product
+  product_code?: string // Product code
+  comment?: string // Line comment
+  comment2?: string // Second line comment
+  price: number // Unit price
+  amount: number // Amount
+  amount2?: number // Additional amount
+  discount?: number // Line discount
+  sum: number // Line sum without VAT
+  vat?: number // Line tax percent
+  unit?: string // Unit name
+  finance_object_id?: number // ID of the related accounting object
+  finance_object_name?: string // Name of the related accounting object
+  finance_object_symbol?: string // Symbol of the related accounting object
+  finance_account_id?: number // ID of the related finance account
+  finance_account_name?: string // Name of the related finance account
+  finance_account_symbol?: string // Symbol of the related finance account
+  project_id?: number // ID of the related project
+  project_name?: string // Name of the related project
+  vat_code_id?: number // Line tax id
+  vat_code?: string // VAT code
+  is_settled?: boolean // If specific Purchase order line is settled (finished) or not
+  custom_fields?: Record<string, unknown> // Custom fields, only filled on view requests
+}
