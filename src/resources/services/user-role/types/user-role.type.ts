19c4e6a3cbc7d5fc895d33ef208c1e4a175b71bb
André Vitor Rampanelli <andre.rampanelli@scoro.com>
2024-04-24 11:38:10 +0300

    feat: add UserRole service

---
+++ src/resources/services/user-role/types/user-role.type.ts
@@ -0,0 +1,7 @@
+import { IUser } from '../../user/types/user.type'
+
+export interface IUserRole {
+  role_id: number // User role ID
+  role_name: string // User role name
+  role_users: Array<Partial<IUser>> // User role users
+}
---
+++ src/resources/services/user-role/user-role.service.ts
@@ -0,0 +1,12 @@
+import { APIClient } from '../../base'
+import { IUserRole } from './types/user-role.type'
+
+export class UserRoleService extends APIClient {
+  async getUserRoles(): Promise<IUserRole[]> {
+    return await this.list<IUserRole>('userRoles')
+  }
+
+  async getUserRoleById(userRoleId: number): Promise<IUserRole> {
+    return await this.view<IUserRole>('userRoles', userRoleId)
+  }
+}
