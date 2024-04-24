98a118c8fad28ff1c07332d5bd85b7a14780e6e8
André Vitor Rampanelli <andre.rampanelli@scoro.com>
2024-04-24 12:02:12 +0300

    feat: add VatCode service

---
+++ src/resources/services/vat-code/types/vat-code.type.ts
@@ -0,0 +1,10 @@
+export interface IVatCode {
+  vat_code_id: number
+  vat_code: string
+  percent: number
+  vat_name: string
+  is_sales: boolean
+  is_purchases: boolean
+  is_active: boolean
+  is_not_applicable: boolean
+}
---
+++ src/resources/services/vat-code/vat-code.service.ts
@@ -0,0 +1,30 @@
+import { APIClient } from '../../base'
+import { IVatCode } from './types/vat-code.type'
+
+export class VatCodeService extends APIClient {
+  async getVatCodes(): Promise<IVatCode[]> {
+    return await this.list<IVatCode>('vatCodes')
+  }
+
+  async findAllVatCodesBy(
+    filters: Record<string, unknown>
+  ): Promise<IVatCode[]> {
+    return await this.list<IVatCode>('vatCodes', filters)
+  }
+
+  async getVatCode(id: number): Promise<IVatCode> {
+    return await this.view<IVatCode>('vatCodes', id)
+  }
+
+  async createVatCode(data: IVatCode): Promise<IVatCode> {
+    return await this.create<IVatCode>('vatCodes', data)
+  }
+
+  async updateVatCode(id: number, data: IVatCode): Promise<IVatCode> {
+    return await this.update<IVatCode>('vatCodes', id, data)
+  }
+
+  async deleteVatCode(id: number): Promise<void> {
+    await this.delete('vatCodes', id)
+  }
+}
