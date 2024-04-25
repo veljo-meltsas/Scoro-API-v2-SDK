56ac9bf45b42110f7b27de640de26941f5567d20
André Vitor Rampanelli <andre.rampanelli@scoro.com>
2024-04-25 14:43:56 +0300

    feat: add RolePrice service

---
+++ src/resources/services/role-price/role-price.service.ts
@@ -0,0 +1,14 @@
+import { APIClient } from '../../base'
+import { IRolePrice } from './types/role-price.type'
+
+export class RolePriceService extends APIClient {
+  async getRolePrices(): Promise<IRolePrice[]> {
+    return await this.list<IRolePrice>('rolesPrices')
+  }
+
+  async findAllRolePricesBy(
+    filters: Record<string, unknown>
+  ): Promise<IRolePrice[]> {
+    return await this.list<IRolePrice>('rolesPrices', filters)
+  }
+}
---
+++ src/resources/services/role-price/types/role-price.type.ts
@@ -0,0 +1,8 @@
+export interface IRolePrice {
+  role_id: number
+  price_list_id: number
+  role_name: string
+  pricelist_name: string
+  selling_price: number
+  currency: string
+}
