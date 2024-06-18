e1a37a3f6d99d2297593febab4ab2b8a8ef69ad9
André Vitor Rampanelli <andrerampanelli1@gmail.com>
2024-06-18 23:07:58 +0300

    fix: add package json as an asset

--- release.config.cjs
+++ release.config.cjs
@@ -18,6 +18,7 @@ module.exports = {
             "@semantic-release/git",
             {
                 "assets": [
+                    "package.json",
                     "CHANGELOG.md"
                 ]
             }
